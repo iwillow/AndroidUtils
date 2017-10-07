7dea8ec12c942391df885d43cbc7db02352feeb7
iwillow <iwillow@163.com>
2017-10-07 23:08:01 +0800

    update some code

--- app/src/main/java/com/iwillow/app/samples/ui/MainActivity.java
+++ app/src/main/java/com/iwillow/app/samples/ui/MainActivity.java
@@ -12,25 +12,25 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    VerticalStepView verticalStepView;
+
     private static final String TAG = MainActivity.class.getSimpleName();
-    //private TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       /* HorizontalStepView horizontalStepView = (HorizontalStepView) findViewById(R.id.horizontalStepView);
+        HorizontalStepView horizontalStepView = (HorizontalStepView) findViewById(R.id.horizontalStepView);
         List<String> data = Arrays.asList(getResources().getStringArray(R.array.step_names));
         horizontalStepView.setItems(data);
-        horizontalStepView.setCurrentStep(0);*/
-        verticalStepView = (VerticalStepView) findViewById(R.id.verticalStepView);
+        horizontalStepView.setCurrentStep(0);
+        VerticalStepView verticalStepView = (VerticalStepView) findViewById(R.id.verticalStepView);
         verticalStepView.setStepItemsListener(new VerticalStepView.StepItemsListener() {
             @Override
             public void onStartLoadItems(VerticalStepView stepView) {
                 List<String> data = Arrays.asList(getResources().getStringArray(R.array.i_have_a_dream));
-                verticalStepView.setItems(data);
-                verticalStepView.finishStep(5);
+                stepView.setItems(data);
+                stepView.finishStep(5);
             }
         });
 
