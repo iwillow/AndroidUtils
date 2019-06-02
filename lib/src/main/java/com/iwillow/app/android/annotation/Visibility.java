912b9b975dc3a311fa7e5c633332f71775061571
iwillow <iwillow@163.com>
2019-06-02 22:27:29 +0800

    add a BaseViewAdapter

---
+++ lib/src/main/java/com/iwillow/app/android/annotation/Visibility.java
@@ -0,0 +1,15 @@
+package com.iwillow.app.android.annotation;
+
+import android.support.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import static android.view.View.GONE;
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
+
+@IntDef({VISIBLE, INVISIBLE, GONE})
+@Retention(RetentionPolicy.SOURCE)
+public @interface Visibility {
+}
---
+++ lib/src/main/java/com/iwillow/app/android/ui/adapter/BaseViewHolder.java
@@ -0,0 +1,324 @@
+package com.iwillow.app.android.ui.adapter;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Bitmap;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.IntDef;
+import android.support.v7.widget.RecyclerView;
+import android.text.util.Linkify;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.widget.CheckBox;
+import android.widget.Checkable;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.iwillow.app.android.annotation.Visibility;
+
+import static android.view.View.GONE;
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
+
+/**
+ * Created by https://github.com/iwillow/ on 2018/5/11.
+ */
+
+
+public class BaseViewHolder extends RecyclerView.ViewHolder {
+    private SparseArray<View> mViews;
+    protected int mPosition;
+    private View mConvertView;
+    private Context mContext;
+    protected int mLayoutId;
+
+    public BaseViewHolder(Context context, View itemView, ViewGroup parent, int position) {
+        super(itemView);
+        mContext = context;
+        mConvertView = itemView;
+        mPosition = position;
+        mViews = new SparseArray<View>();
+        mConvertView.setTag(this);
+    }
+
+    public BaseViewHolder(Context context, View itemView) {
+        super(itemView);
+        mContext = context;
+        mConvertView = itemView;
+        mViews = new SparseArray<View>();
+    }
+
+    public static BaseViewHolder createViewHolder(Context context, View itemView) {
+        return new BaseViewHolder(context, itemView);
+    }
+
+    public static BaseViewHolder createViewHolder(Context context, ViewGroup parent, int layoutId) {
+        View itemView = LayoutInflater.from(context).inflate(layoutId, parent,
+                false);
+        return new BaseViewHolder(context, itemView);
+    }
+
+    public static BaseViewHolder get(Context context, View convertView,
+                                     ViewGroup parent, int layoutId, int position) {
+        if (convertView == null) {
+            View itemView = LayoutInflater.from(context).inflate(layoutId, parent,
+                    false);
+            BaseViewHolder holder = new BaseViewHolder(context, itemView, parent, position);
+            holder.mLayoutId = layoutId;
+            return holder;
+        } else {
+            BaseViewHolder holder = (BaseViewHolder) convertView.getTag();
+            holder.mPosition = position;
+            return holder;
+        }
+    }
+
+
+    public <T extends View> T getView(int viewId) {
+        View view = mViews.get(viewId);
+        if (view == null) {
+            view = mConvertView.findViewById(viewId);
+            mViews.put(viewId, view);
+        }
+        return (T) view;
+    }
+
+    public View getConvertView() {
+        return mConvertView;
+    }
+
+    public int getLayoutId() {
+        return mLayoutId;
+    }
+
+    public void updatePosition(int position) {
+        mPosition = position;
+    }
+
+    public int getItemPosition() {
+        return mPosition;
+    }
+
+
+    public BaseViewHolder setText(int viewId, String text) {
+        TextView tv = getView(viewId);
+        tv.setText(text);
+        return this;
+    }
+
+    public BaseViewHolder setText(int viewId, CharSequence text) {
+        TextView tv = getView(viewId);
+        tv.setText(text);
+        return this;
+    }
+
+    public BaseViewHolder setTextSize(int viewId, int unit, float textSize) {
+        TextView tv = getView(viewId);
+        tv.setTextSize(unit, textSize);
+        return this;
+    }
+
+    public BaseViewHolder setCompoundDrawablesWithIntrinsicBounds(int viewId, int left, int top, int right, int bottom) {
+        TextView tv = getView(viewId);
+        tv.setCompoundDrawablesWithIntrinsicBounds(left, top, right, bottom);
+        return this;
+    }
+
+    public BaseViewHolder setImageResource(int viewId, int resId) {
+        ImageView view = getView(viewId);
+        view.setImageResource(resId);
+        return this;
+    }
+
+    public BaseViewHolder setImageBitmap(int viewId, Bitmap bitmap) {
+        ImageView view = getView(viewId);
+        view.setImageBitmap(bitmap);
+        return this;
+    }
+
+    public BaseViewHolder setImageDrawable(int viewId, Drawable drawable) {
+        ImageView view = getView(viewId);
+        view.setImageDrawable(drawable);
+        return this;
+    }
+
+    public BaseViewHolder setBackgroundColor(int viewId, int color) {
+        View view = getView(viewId);
+        view.setBackgroundColor(color);
+        return this;
+    }
+
+    public BaseViewHolder setBackgroundRes(int viewId, int backgroundRes) {
+        View view = getView(viewId);
+        view.setBackgroundResource(backgroundRes);
+        return this;
+    }
+
+    public BaseViewHolder setBackgroundDrawable(int viewId, Drawable background) {
+        View view = getView(viewId);
+        view.setBackgroundDrawable(background);
+        return this;
+    }
+
+    public BaseViewHolder setButtonDrawable(int viewId, int drawableId) {
+        CheckBox checkBox = getView(viewId);
+        checkBox.setButtonDrawable(drawableId);
+        return this;
+    }
+
+    public BaseViewHolder setTextColor(int viewId, int textColor) {
+        TextView view = getView(viewId);
+        view.setTextColor(textColor);
+        return this;
+    }
+
+    public BaseViewHolder setTextColorRes(int viewId, int textColorRes) {
+        TextView view = getView(viewId);
+        view.setTextColor(mContext.getResources().getColor(textColorRes));
+        return this;
+    }
+
+    public BaseViewHolder setTextColorStateList(int viewId, ColorStateList colors) {
+        TextView view = getView(viewId);
+        view.setTextColor(colors);
+        return this;
+    }
+
+    @SuppressLint("NewApi")
+    public BaseViewHolder setAlpha(int viewId, float value) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            getView(viewId).setAlpha(value);
+        } else {
+            // Pre-honeycomb hack to set Alpha value
+            AlphaAnimation alpha = new AlphaAnimation(value, value);
+            alpha.setDuration(0);
+            alpha.setFillAfter(true);
+            getView(viewId).startAnimation(alpha);
+        }
+        return this;
+    }
+
+    public BaseViewHolder setVisible(int viewId, boolean visible) {
+        View view = getView(viewId);
+        view.setVisibility(visible ? VISIBLE : GONE);
+        return this;
+    }
+    public BaseViewHolder setVisible(int viewId,@Visibility int visibility) {
+        View view = getView(viewId);
+        view.setVisibility(visibility);
+        return this;
+    }
+
+    public BaseViewHolder linkify(int viewId) {
+        TextView view = getView(viewId);
+        Linkify.addLinks(view, Linkify.ALL);
+        return this;
+    }
+
+    public BaseViewHolder setTypeface(Typeface typeface, int... viewIds) {
+        for (int viewId : viewIds) {
+            TextView view = getView(viewId);
+            view.setTypeface(typeface);
+            view.setPaintFlags(view.getPaintFlags() | Paint.SUBPIXEL_TEXT_FLAG);
+        }
+        return this;
+    }
+
+    public BaseViewHolder setProgress(int viewId, int progress) {
+        ProgressBar view = getView(viewId);
+        view.setProgress(progress);
+        return this;
+    }
+
+    public BaseViewHolder setProgress(int viewId, int progress, int max) {
+        ProgressBar view = getView(viewId);
+        view.setMax(max);
+        view.setProgress(progress);
+        return this;
+    }
+
+    public BaseViewHolder setMax(int viewId, int max) {
+        ProgressBar view = getView(viewId);
+        view.setMax(max);
+        return this;
+    }
+
+    public BaseViewHolder setRating(int viewId, float rating) {
+        RatingBar view = getView(viewId);
+        view.setRating(rating);
+        return this;
+    }
+
+    public BaseViewHolder setRating(int viewId, float rating, int max) {
+        RatingBar view = getView(viewId);
+        view.setMax(max);
+        view.setRating(rating);
+        return this;
+    }
+
+    /**
+     * 使用时注意，此方法不可以给list的item设置，
+     * 因为当view复用时获取tag会发生类型不匹配异常
+     *
+     * @param viewId 控件id(非item控件)
+     * @param tag    tag
+     * @return BaseViewHolder
+     */
+    public BaseViewHolder setTag(int viewId, Object tag) {
+        View view = getView(viewId);
+        view.setTag(tag);
+        return this;
+    }
+
+    public BaseViewHolder setTag(int viewId, int key, Object tag) {
+        View view = getView(viewId);
+        view.setTag(key, tag);
+        return this;
+    }
+
+    public BaseViewHolder setSelected(int viewId, boolean selected) {
+        View view = getView(viewId);
+        view.setSelected(selected);
+        return this;
+    }
+
+    public BaseViewHolder setChecked(int viewId, boolean checked) {
+        Checkable view = (Checkable) getView(viewId);
+        view.setChecked(checked);
+        return this;
+    }
+
+    public BaseViewHolder setEnable(int viewId, boolean enable) {
+        View view = getView(viewId);
+        view.setEnabled(enable);
+        return this;
+    }
+
+    public BaseViewHolder setOnClickListener(int viewId, View.OnClickListener listener) {
+        View view = getView(viewId);
+        view.setOnClickListener(listener);
+        return this;
+    }
+
+    public BaseViewHolder setOnTouchListener(int viewId, View.OnTouchListener listener) {
+        View view = getView(viewId);
+        view.setOnTouchListener(listener);
+        return this;
+    }
+
+    public BaseViewHolder setOnLongClickListener(int viewId, View.OnLongClickListener listener) {
+        View view = getView(viewId);
+        view.setOnLongClickListener(listener);
+        return this;
+    }
+
+}
